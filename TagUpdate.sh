d791c66696a46ed7d24115c01cc4609267aea7dd
Bharathi3692 <98962265+Bharathi3692@users.noreply.github.com>
2022-03-15 19:18:20 +0530

    Add files via upload

---
+++ TagUpdate.sh
@@ -0,0 +1,16 @@
+for sub_name in ${sub}
+do
+	az account set --name "$sub_name"
+	group_id=$(az group list --subscription "$sub_name" --query [].id --output tsv)
+   for RG_Id in ${group_id}
+   do
+	az tag update --resource-id $RG_Id --operation Merge --tags "$Tag_Name"="$Tag_Value"
+	group=$(az group list --query "[?id=='$RG_Id'].name" --output tsv)
+	res_id=$(az resource list --resource-group $group --query [].id --output tsv)
+   for Rs_Id in ${res_id}
+   do
+	az tag update --resource-id $Rs_Id --operation Merge --tags "$Tag_Name"="$Tag_Value"
+   done
+   done
+done
+
